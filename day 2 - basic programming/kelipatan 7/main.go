d88375644172309312c17baef523f65e52b5b473
unknown <wulandariiratnaa@gmail.com>
2023-04-05 10:03:05 +0700

    tugas day 2 mengenai basic programming

--- day 2 - basic programming/kelipatan 7/main.go
+++ day 2 - basic programming/kelipatan 7/main.go
@@ -3,13 +3,21 @@ package main
 import "fmt"
 
 func main() {
-	var n int
-	fmt.Print("Masukkan angka: ")
-	fmt.Scanln(&n)
+	// var n int
+	// fmt.Print("Masukkan angka: ")
+	// fmt.Scanln(&n)
 
-	if n%7 == 0 {
-		fmt.Println("bilangan kelipatan 7")
-	} else {
-		fmt.Println("bukan bilangan kelipatan 7")
+	// if n%7 == 0 {
+	// 	fmt.Println("bilangan kelipatan 7")
+	// } else {
+	// 	fmt.Println("bukan bilangan kelipatan 7")
+	// }
+
+	for i := 7; i <= 15; i += 7 {
+
+		if i%7 == 0 {
+			fmt.Println(i)
+		}
 	}
+
 }
